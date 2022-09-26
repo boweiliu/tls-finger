b409d72c3af5f592305f84c9b0701cfca742e9a9
Bowei Liu <liubowei@gmail.com>
2022-09-26 07:10:04 +0000

    wip grrrr

--- hex.py
+++ hex.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # usage: socat -x -lu tcp-listen:9090,reuseaddr,fork tcp:www.google.com:443 2>&1 | tee /tmp/socat | ./hex.py | xxd | vi -R - 
-out=''
-with open('/dev/stdin') as f:
-    for line in f:
-        if line[0] == '>' or line[0] == '<':
-            continue
-        for s in line.split(' '):
-            if s == '':
-                continue
-            out += chr(int('0x' + s, 0))
+#out=''
 with open('/dev/stdout', 'w') as g:
-    g.write(out)
+    with open('/dev/stdin') as f:
+        for line in f:
+            if line[0] == '>' or line[0] == '<':
+                continue
+            for s in line.split(' '):
+                if s == '':
+                    continue
+                #out += chr(int('0x' + s, 0))
+                g.write(chr(int('0x' + s, 0)))
+                g.flush()
+        #g.write(out)
--- l2r.sh
+++ l2r.sh
@@ -1,5 +1,10 @@
 
 #!/bin/bash
 # consumes from stdin and writes to stdout
+# turn off line buffering: stdbuf -i0 -o0 -e0 command
 
-tee l2r | xxd | tee l2r.hex | xxd -r
+#tee l2r | stdbuf -i0 -o0 -e0 xxd | tee l2r.hex | stdbuf -i0 -o0 -e0 xxd -r
+#tee l2r | tee /dev/null | tee /dev/null
+#tee l2r
+#tee l2r | stdbuf -i0 -o0 -e0 hexdump -v -e '/1 "%02X\n"' | tee l2r.hex | stdbuf -i0 -o0 -e0 ./unhex.py
+stdbuf -i0 -o0 -e0 hexdump -v -e '/1 "%02X\n"' | tee l2r.hex | stdbuf -i0 -o0 -e0 ./unhex.py
