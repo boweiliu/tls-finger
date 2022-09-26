7b8000765375b31eb2d13a7b97c8498a574aabeb
Bowei Liu <liubowei@gmail.com>
2022-09-26 07:13:32 +0000

    fixup python

--- hex.py
+++ hex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # usage: socat -x -lu tcp-listen:9090,reuseaddr,fork tcp:www.google.com:443 2>&1 | tee /tmp/socat | ./hex.py | xxd | vi -R - 
 #out=''
-with open('/dev/stdout', 'w') as g:
+with open('/dev/stdout', 'wb') as g:
     with open('/dev/stdin') as f:
         for line in f:
             if line[0] == '>' or line[0] == '<':
@@ -10,6 +10,6 @@ with open('/dev/stdout', 'w') as g:
                 if s == '':
                     continue
                 #out += chr(int('0x' + s, 0))
-                g.write(chr(int('0x' + s, 0)))
+                g.write(int('0x' + s, 0).to_bytes(1, 'little'))
                 g.flush()
         #g.write(out)
